72ce8feb6f4b2a4ffd71862b56ee363742f1f40d
muxiafeng <862365198@qq.com>
2024-07-23 16:54:54 +0800

    宏志项目测试用例

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def run():
         # 判断现有的测试用例，如果未生成测试代码，则自动生成
         # TestCaseAutomaticGeneration().get_case_automatic()
 
-        pytest.main(['-s', '-W', 'ignore:Module already imported:pytest.PytestWarning',
+        pytest.main(['-q', '-W', 'ignore:Module already imported:pytest.PytestWarning',
                      '--alluredir', './report/tmp', "--clean-alluredir"])
 
         """
